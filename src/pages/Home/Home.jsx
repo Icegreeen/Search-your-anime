a10c1ef6719df07a5fb9096e140e31094c1f6914
Icegreeen <flavioaquila16@gmail.com>
2021-09-27 11:20:42 -0300

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+import SearchInput from '../../components/SearchInput/SearchInput';
+import './styles.css';
+
+
+const api = 'https://kitsu.io/api/edge/'
+
+export default function Home() {
+    const [info, setInfo] = useState({});
+    const [ text, setText ] = useState('');
+    
+    useEffect(() => {
+        if (text) {
+            fetch(`${api}anime?filter[text]=${text}&page[limit]=10`)
+                .then((res) => res.json())
+                .then((res) => {
+                    setInfo(res);
+                })
+        }
+    }, [text]);
+
+    return (
+       <div className="container">
+           <h1>Search for your favorite anime.</h1>
+
+            <SearchInput 
+                value={text} 
+                onChange={(search) => setText(search)} 
+            />
+
+                <div className="animes">
+                    {info.data && (
+                        <ul className="animeList">
+                            {info.data.map((anime) => (
+                                <li key={anime.id}>
+                                    <img 
+                                        src={anime.attributes.posterImage.small} 
+                                        alt={anime.attributes.canonicalTitle}
+                                    />
+
+                                    {anime.attributes.canonicalTitle}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+                
+        </div>  
+    );
+}
